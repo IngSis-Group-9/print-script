775e83eb9cddf862ee91d1c6ca23ccb66bc04a4c
pedrojschmidt <pedro.schmidt@ing.austral.edu.ar>
2024-06-28 10:53:04 -0300

    ConditionalFormatter improved

--- formatter/src/main/kotlin/formatter/formatters/ConditionalFormatter.kt
+++ formatter/src/main/kotlin/formatter/formatters/ConditionalFormatter.kt
@@ -1,7 +1,12 @@
 package formatter.formatters
 
 import ast.ASTNode
+import ast.BinaryOperation
 import ast.Conditional
+import ast.Declaration
+import ast.DeclarationAssignation
+import ast.Method
+import ast.SimpleAssignation
 import formatter.FormatRules
 
 class ConditionalFormatter : Formatter {
@@ -13,8 +18,14 @@ class ConditionalFormatter : Formatter {
         val indentSize = rules.nSpacesIndentationForIfStatement
         val tabs = "\t".repeat(indentSize)
 
-        val formattedThen = conditional.then.joinToString("") { MethodFormatter().formatNode(it, rules).replaceIndent(tabs) }
-        val formattedOtherwise = conditional.otherwise?.joinToString("") { MethodFormatter().formatNode(it, rules).replaceIndent(tabs) } ?: ""
+        val formattedThen =
+            conditional.then.joinToString("") {
+                getFormatterForNode(it).formatNode(it, rules).replaceIndent(tabs)
+            }
+        val formattedOtherwise =
+            conditional.otherwise?.joinToString("") {
+                getFormatterForNode(it).formatNode(it, rules).replaceIndent(tabs)
+            } ?: ""
 
         return buildString {
             append("if (")
@@ -28,6 +39,18 @@ class ConditionalFormatter : Formatter {
                 append("\n}")
             }
             append("\n")
+        }
+    }
+
+    fun getFormatterForNode(node: ASTNode): Formatter {
+        return when (node) {
+            is Declaration -> DeclarationFormatter()
+            is DeclarationAssignation -> DeclarationAssignationFormatter()
+            is SimpleAssignation -> SimpleAssignationFormatter()
+            is Method -> MethodFormatter()
+            is BinaryOperation -> BinaryOperationFormatter()
+            is Conditional -> ConditionalFormatter()
+            else -> throw IllegalArgumentException("No formatter found for node type ${node::class}")
         }
     }
 }
